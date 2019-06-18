4c99f7b07a095fc97f69e1fbd114ddf1b174381b
d2gex <daniel.garcia@d2garcia.com>
2019-06-18 11:10:35 +0100

    Add email,name,description/is_allowed fields to application model

--- authorization_server/models.py
+++ authorization_server/models.py
@@ -23,7 +23,11 @@ class Application(db.Model):
     __tablename__ = 'application'
     id = db.Column(db.String(length=40), primary_key=True, nullable=False)
     reg_token = db.Column(db.String(length=40), unique=True)
+    email = db.Column(db.String(length=50), unique=True, nullable=False)
+    name = db.Column(db.String(length=50), nullable=False)
+    description = db.Column(db.String(length=255), nullable=False)
     password = db.Column(db.String(length=128))
     active = db.Column(db.Boolean, default=True)
+    is_allowed = db.Column(db.Boolean, default=False)
     created = db.Column(db.DateTime, default=datetime.now)
     updated = db.Column(db.DateTime)
--- tests/test_db.py
+++ tests/test_db.py
@@ -34,13 +34,22 @@ def test_user_table():
         raise AssertionError('user_2 did not throw Integrity error as expected')
 
 
+@test_utils.reset_database(tear='up_down')
 def test_client_table():
-    ''' Test that client_id as string primary key and reg_token are both unique and don't accept duplicates
+    ''' Test that application table respect the following constraints:
+
+    1) client_id is unique
+    2) name and description cannot be nullable
+    3) reg_token is unique and nullable
+    4) email is unique and not nullable
     '''
 
+    # (1)
     app_id = str(uuid.uuid4()).replace('-', '')
-    client_1 = models.Application(id=app_id, password='something')
-    client_2 = models.Application(id=app_id, password='something')
+    client_1 = models.Application(id=app_id, name='client_1', description='Client 1 ...',
+                                  password='something', email='email_a@example.com')
+    client_2 = models.Application(id=app_id, name='client_2', description='Client 2 ...',
+                                  password='something', email='email_b@example.com')
     db.session.add(client_1)
     db.session.commit()
     assert db.session.query(models.Application).one()
@@ -52,18 +61,60 @@ def test_client_table():
     except exc.IntegrityError:
         db.session.rollback()
     else:
-        raise AssertionError('client_2 did not throw Integrity error as expected')
-    client_3 = models.Application(id=str(uuid.uuid4()).replace('-', ''), password='something', reg_token='sametoken')
-    client_4 = models.Application(id=str(uuid.uuid4()).replace('-', ''), password='something', reg_token='sametoken')
+        raise AssertionError('client_2 did not throw Integrity error as expected when client_id is not  unique')
+
+    # (2.1)
+    # --> Name cannot be null
+    client_2.id = 'something else'
+    client_2.name = None
+    db.session.add(client_2)
+    try:
+        db.session.commit()
+    except exc.OperationalError:
+        db.session.rollback()
+    else:
+        raise AssertionError('client_2 did not throw Operation error as expected when name is null')
+
+    # (2.2)
+    # --> Description cannot be null
+    client_2.name = 'something'
+    client_2.description = None
+    db.session.add(client_2)
+    try:
+        db.session.commit()
+    except exc.OperationalError:
+        db.session.rollback()
+    else:
+        raise AssertionError('client_2 did not throw Operation error as expected when description is null')
+
+    # (3)
+    # ---> reg_token need to be unique
+    client_3 = models.Application(id=str(uuid.uuid4()).replace('-', ''), password='something', reg_token='sametoken',
+                                  email='email_c@example.com', name='client 3', description='client 3 ...')
+    client_4 = models.Application(id=str(uuid.uuid4()).replace('-', ''), password='something', reg_token='sametoken',
+                                  email='email_d@example.com', name='client 4', description='client 4 ...')
     db.session.add(client_3)
     db.session.commit()
-    assert db.session.query(func.count(models.Application.id)).first()[0] == 2
     db.session.add(client_4)
     try:
         db.session.commit()
     except exc.IntegrityError:
         db.session.rollback()
     else:
-        raise AssertionError('client_4 did not throw Integrity error as expected')
+        raise AssertionError('client_4 did not throw Integrity error as expected when reg_token is not unique')
 
-
+    # (4)
+    # --> email need to be unique
+    client_5 = models.Application(id=str(uuid.uuid4()).replace('-', ''), password='something', reg_token='token_1',
+                                  email='email_e@example.com', name='client 5', description='client 5 ...')
+    client_6 = models.Application(id=str(uuid.uuid4()).replace('-', ''), password='something', reg_token='token_2',
+                                  email='email_e@example.com', name='client 6', description='client 6 ...')
+    db.session.add(client_5)
+    db.session.commit()
+    db.session.add(client_6)
+    try:
+        db.session.commit()
+    except exc.IntegrityError:
+        db.session.rollback()
+    else:
+        raise AssertionError('client_6 did not throw Integrity error as expected when email is not unique')
