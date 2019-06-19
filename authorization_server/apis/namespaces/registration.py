d0d25ae0d633b9109c8b3ca92be9e7893c6a0baf
d2gex <daniel.garcia@d2garcia.com>
2019-06-19 13:38:04 +0100

    Refactor apis' utils module importing to be used as api_utils

--- authorization_server/apis/namespaces/registration.py
+++ authorization_server/apis/namespaces/registration.py
@@ -4,7 +4,7 @@ from flask_restplus import Resource, fields
 from authorization_server import models
 from authorization_server.app import db
 from authorization_server.apis.namespace import NameSpace
-from authorization_server.apis import utils, errors as api_errors
+from authorization_server.apis import utils as api_utils, errors as api_errors
 
 
 api = NameSpace('registration', description="Resource used by clients to register with the Authorisation server")
@@ -20,9 +20,9 @@ registration_dto = api.model('Registration', {
 class Registration(Resource):
 
     @api.expect(registration_dto)
-    @api.response_error(api_errors.BadRequest400Error(message=utils.RESPONSE_400))
-    @api.response_error(api_errors.Conflict409Error(message=utils.RESPONSE_409))
-    @api.response(201, json.dumps(utils.RESPONSE_201_REGISTRATION_POST), body=False)
+    @api.response_error(api_errors.BadRequest400Error(message=api_utils.RESPONSE_400))
+    @api.response_error(api_errors.Conflict409Error(message=api_utils.RESPONSE_409))
+    @api.response(201, json.dumps(api_utils.RESPONSE_201_REGISTRATION_POST), body=False)
     def post(self):
         '''Register a new application client
         '''
@@ -30,19 +30,19 @@ class Registration(Resource):
         if not isinstance(api.payload, dict):
             raise api_errors.BadRequest400Error(
                 message='Incorrect type of object received. Instead a json object is expected',
-                envelop=utils.RESPONSE_400)
+                envelop=api_utils.RESPONSE_400)
 
         # Do we have all expected fields?
         expected_fields = registration_dto.keys()
         for key in expected_fields:
             if key not in api.payload:
                 raise api_errors.BadRequest400Error(message=f"Required key '{key}' not found",
-                                                    envelop=utils.RESPONSE_400)
+                                                    envelop=api_utils.RESPONSE_400)
 
         # has the client already registered?
         if db.session.query(models.Application).filter_by(email=api.payload['email']).first():
             raise api_errors.Conflict409Error(message=f"Email '{api.payload['email']}' has already been registered",
-                                              envelop=utils.RESPONSE_409)
+                                              envelop=api_utils.RESPONSE_409)
 
         # Let's register the client
         data = api.payload
@@ -50,6 +50,6 @@ class Registration(Resource):
         db.session.add(models.Application(**data))
         db.session.commit()
 
-        response = dict(utils.RESPONSE_201_REGISTRATION_POST)
+        response = dict(api_utils.RESPONSE_201_REGISTRATION_POST)
         response['id'] = data['id']
         return response, 201
--- authorization_server/apis/namespaces/verification.py
+++ authorization_server/apis/namespaces/verification.py
@@ -6,7 +6,7 @@ from flask_restplus import Resource, fields
 from authorization_server import models
 from authorization_server.app import db, bcrypt
 from authorization_server.apis.namespace import NameSpace
-from authorization_server.apis import utils, errors as api_errors
+from authorization_server.apis import utils as api_utils, errors as api_errors
 
 
 api = NameSpace('verification', description="Resource used by clients to verify their registration with the "
@@ -22,9 +22,9 @@ verification_dto = api.model('Verification', {
 class Verification(Resource):
 
     @api.expect(verification_dto)
-    @api.response_error(api_errors.BadRequest400Error(message=utils.RESPONSE_400))
-    @api.response_error(api_errors.Conflict409Error(message=utils.RESPONSE_409))
-    @api.response(201, json.dumps(utils.RESPONSE_201_VERIFICATION_POST), body=False)
+    @api.response_error(api_errors.BadRequest400Error(message=api_utils.RESPONSE_400))
+    @api.response_error(api_errors.Conflict409Error(message=api_utils.RESPONSE_409))
+    @api.response(201, json.dumps(api_utils.RESPONSE_201_VERIFICATION_POST), body=False)
     def post(self):
         '''Register a new application client
         '''
@@ -32,14 +32,14 @@ class Verification(Resource):
         if not isinstance(api.payload, dict):
             raise api_errors.BadRequest400Error(
                 message='Incorrect type of object received. Instead a json object is expected',
-                envelop=utils.RESPONSE_400)
+                envelop=api_utils.RESPONSE_400)
 
         # Do we have all expected fields?
         expected_fields = verification_dto.keys()
         for key in expected_fields:
             if key not in api.payload:
                 raise api_errors.BadRequest400Error(message=f"Required key '{key}' not found",
-                                                    envelop=utils.RESPONSE_400)
+                                                    envelop=api_utils.RESPONSE_400)
 
         # Client should exist, should not have verified before and the token should match the one stored in the db
         try:
@@ -51,15 +51,15 @@ class Verification(Resource):
             raise api_errors.Conflict409Error(message=f"Client '{api.payload['id']}' may not yet have registered "
                                                   f"or token is invalid. Please register first at "
                                                   f"{request.url.replace('verification', 'registration')}",
-                                              envelop=utils.RESPONSE_409)
+                                              envelop=api_utils.RESPONSE_409)
         else:
-            password = utils.generate_password(10)
+            password = api_utils.generate_password(10)
             db_data.password = bcrypt.generate_password_hash(password).decode('utf-8')
             db_data.token = None
             db.session.add(db_data)
             db.session.commit()
 
-            response = dict(utils.RESPONSE_201_VERIFICATION_POST)
+            response = dict(api_utils.RESPONSE_201_VERIFICATION_POST)
             response['id'] = db_data.id
             response['password'] = password
             return response, 201
