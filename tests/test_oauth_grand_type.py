b70942f9a57d89078a9602fe6fa55bb3772b87b2
d2gex <daniel.garcia@d2garcia.com>
2019-06-25 11:21:00 +0100

    Refactor Authorisation Code Request for better error logic

--- tests/test_oauth_grand_type.py
+++ tests/test_oauth_grand_type.py
@@ -12,62 +12,40 @@ from tests import utils as test_utils
 def test_valid_request():
     '''Ensure that a request is valid as follows:
 
-    1) client_id is a required argument
-    2) response_type is required and must be supported
-    3) state is required and must valid
-    4) client_id does not exist in the database
-    5) redirect_uri, if provided, must match the one stored in the database
-    6) Otherwise return True
+    1) client_id is a required argument and must exist in the database
+    2) redirect_uri, if provided, must match the one stored in the database
+    3) response_type is required and must be supported
+    4) state is required and must valid
+    5) Otherwise return True
     '''
 
     kwargs = {}
+
     # (1.1)
     auth_code = oauth_gt.AuthorisationCode(**kwargs)
     errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.RO_TYPE_ERROR
-    assert errors['error'] == oauth_gt.RO_CLIENT_ID_ERROR
+    assert errors['addressee'] == oauth_gt.RESOURCE_OWNER_ERROR
+    assert errors['error'] is None
     assert 'invalid identifier' in errors['error_description']
 
+    # (1.2)
     kwargs['client_id'] = ''
     auth_code = oauth_gt.AuthorisationCode(**kwargs)
     errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.RO_TYPE_ERROR
-    assert errors['error'] == oauth_gt.RO_CLIENT_ID_ERROR
+    assert errors['addressee'] == oauth_gt.RESOURCE_OWNER_ERROR
+    assert errors['error'] is None
     assert 'invalid identifier' in errors['error_description']
 
-    # (2.1)
+    # (1.3)
     kwargs['client_id'] = 'something'
     auth_code = oauth_gt.AuthorisationCode(**kwargs)
     errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.CLIENT_TYPE_ERROR
-    assert errors['error'] == oauth_gt.CLIENT_UNSUPPORTED_RESPONSE_TYPE_ERROR
-    assert 'response_type' in errors['error_description']
+    assert errors['addressee'] == oauth_gt.RESOURCE_OWNER_ERROR
+    assert errors['error'] is None
+    assert 'is not registered with us' in errors['error_description']
 
-    kwargs['response_type'] = 'unexpected value'
-    auth_code = oauth_gt.AuthorisationCode(**kwargs)
-    errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.CLIENT_TYPE_ERROR
-    assert errors['error'] == oauth_gt.CLIENT_UNSUPPORTED_RESPONSE_TYPE_ERROR
-    assert 'response_type' in errors['error_description']
-
-    # (3.1)
-    kwargs['response_type'] = oauth_gt.AuthorisationCode.grand_type
-    auth_code = oauth_gt.AuthorisationCode(**kwargs)
-    errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.CLIENT_TYPE_ERROR
-    assert errors['error'] == oauth_gt.CLIENT_INVALID_REQUEST_ERROR
-    assert 'state' in errors['error_description']
-
-    # (3.2)
-    kwargs['state'] = "    "
-    auth_code = oauth_gt.AuthorisationCode(**kwargs)
-    errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.CLIENT_TYPE_ERROR
-    assert errors['error'] == oauth_gt.CLIENT_INVALID_REQUEST_ERROR
-    assert 'state' in errors['error_description']
-
-    # (4)
-    kwargs['state'] = 'something'
+    # (2)
+    # --> Pre-fill the database with some client data
     constraints = {
         'id': True,
         'email': True,
@@ -80,34 +58,53 @@ def test_valid_request():
     data = test_utils.generate_pair_client_model_data(constraints)
     db.session.add(models.Application(**data[0]))
     db.session.commit()
-    db.session.query(models.Application).one()  # It will throw an exception if there is no exactly one row in the table
-    auth_code = oauth_gt.AuthorisationCode(**kwargs)
-    errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.RO_TYPE_ERROR
-    assert errors['error'] == oauth_gt.RO_CLIENT_ID_ERROR
-    assert 'not registered with us' in errors['error_description']
+    # It will throw an exception if there is no exactly one row in the table
+    db_data = db.session.query(models.Application).one()
 
-    # (5.1)
-    kwargs['client_id'] = data[0]['id']
+    # -->(2.1)
+    kwargs['client_id'] = db_data.id
     kwargs['redirect_uri'] = 'this is not a base64url encoded'
     auth_code = oauth_gt.AuthorisationCode(**kwargs)
     errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.CLIENT_TYPE_ERROR
-    assert errors['error'] == oauth_gt.CLIENT_INVALID_REQUEST_ERROR
-    assert 'redirect_uri' in errors['error_description']
-    assert 'does not match the one' not in errors['error_description']
+    assert errors['addressee'] == oauth_gt.RESOURCE_OWNER_ERROR
+    assert errors['error'] is None
+    assert all([words in errors['error_description']] for words in ('redirect_uri', 'does not match the one'))
 
-    # (5.2)
-    kwargs['client_id'] = data[0]['id']
-    kwargs['redirect_uri'] = base64.urlsafe_b64encode(b'this is not a base64url encoded').decode()
+    # --> (2.2)
+    kwargs['redirect_uri'] = base64.urlsafe_b64encode(b'https://www.donotexist.com').decode()
     auth_code = oauth_gt.AuthorisationCode(**kwargs)
     errors = auth_code.validate_request()
-    assert errors['type'] == oauth_gt.CLIENT_TYPE_ERROR
-    assert errors['error'] == oauth_gt.CLIENT_INVALID_REQUEST_ERROR
-    assert all([keyword in errors['error_description']] for keyword in ('redirect_uri', 'does not match the one'))
+    assert errors['addressee'] == oauth_gt.RESOURCE_OWNER_ERROR
+    assert errors['error'] is None
+    assert all([words in errors['error_description']] for words in ('redirect_uri', 'is not registered with us'))
 
-    # (6)
-    kwargs['redirect_uri'] = base64.urlsafe_b64encode(data[0]['redirect_uri'].encode()).decode()
+    # (3)
+    kwargs['redirect_uri'] = base64.urlsafe_b64encode(db_data.redirect_uri.encode()).decode()
+    kwargs['response_type'] = 'unexpected value'
+    auth_code = oauth_gt.AuthorisationCode(**kwargs)
+    errors = auth_code.validate_request()
+    assert errors['addressee'] == oauth_gt.CLIENT_ERROR
+    assert errors['error'] == oauth_gt.CLIENT_UNSUPPORTED_RESPONSE_TYPE_ERROR
+    assert 'response_type' in errors['error_description']
+
+    # (4.1)
+    kwargs['response_type'] = oauth_gt.AuthorisationCode.grand_type
+    auth_code = oauth_gt.AuthorisationCode(**kwargs)
+    errors = auth_code.validate_request()
+    assert errors['addressee'] == oauth_gt.CLIENT_ERROR
+    assert errors['error'] == oauth_gt.CLIENT_INVALID_REQUEST_ERROR
+    assert 'state' in errors['error_description']
+
+    # (4.2)
+    kwargs['state'] = "    "
+    auth_code = oauth_gt.AuthorisationCode(**kwargs)
+    errors = auth_code.validate_request()
+    assert errors['addressee'] == oauth_gt.CLIENT_ERROR
+    assert errors['error'] == oauth_gt.CLIENT_INVALID_REQUEST_ERROR
+    assert 'state' in errors['error_description']
+
+    # (5)
+    kwargs['state'] = 'something'
     auth_code = oauth_gt.AuthorisationCode(**kwargs)
     success = auth_code.validate_request()
     assert success is True
