c5e343d0b92fd715fc32653b88d0b4c49bf3ca64
d2gex <daniel.garcia@d2garcia.com>
2019-06-26 12:22:55 +0100

    Add custom login_required decorator

--- authorization_server/utils.py
+++ authorization_server/utils.py
@@ -1,5 +1,31 @@
+import functools
+from flask import request, current_app, redirect, url_for
+from flask_login import current_user
+
+
 def init_class(cls):
     '''Decorator that initialise a CLASS object after this has been created
     '''
     cls.init()
     return cls
+
+
+def login_required(route, request_args=False, **additional_args):
+    '''A decorator that works similarly to flask_login.login_required to allow custom target and query_string redirection.
+
+    :param route: method of a particular view to be redirected to
+    :param request_args: boolean flag that tells if all url parameters of request.args should be passed on
+    :param additional_args: url arguments to be passed to the route
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if current_app.config.get('LOGIN_DISABLED'):
+                return func(*args, **kwargs)
+            if not current_user.is_authenticated:
+                query_string = {key: value for key, value in request.args.items()} if request_args else {}
+                query_string.update(additional_args)
+                return redirect(url_for(route, **query_string))
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 from authorization_server import utils
+from unittest.mock import patch, MagicMock
 
 
 def test_init_class():
@@ -13,3 +14,76 @@ def test_init_class():
             cls.x = 10
 
     assert Stub.x == 10
+
+
+def test_login_required():
+    '''Ensure the decorator works as follows:
+
+    1)  if LOGIN_DISABLED is actually set to true => the decorated function is called
+    2)  if LOGIN_DISABLED is set to False and user is authenticated => the decorated function is called
+    3)  Otherwise redirect user to provided route with the given query_string parameters, including those in
+        request.args if request_args=True
+    4) Same as 3) but only the additional query_string parameters are passed when request_args=False
+    '''
+
+    fake_view_called = 'yes'
+    additional_arguments = {'another_argument': 'something'}
+    route = 'blueprint.route'
+
+    # (1)
+    with patch.object(utils.current_app.config, 'get', return_value=True):
+        with patch.object(utils, 'current_user') as mock_current_user:
+            @utils.login_required(route, request_args=True, **additional_arguments)
+            def fake_view():
+                return fake_view_called
+
+            ret = fake_view()
+            assert ret == fake_view_called
+            mock_current_user.assert_not_called()
+
+    # (2)
+    with patch.object(utils.current_app.config, 'get', return_value=False):
+        with patch.object(utils, 'current_user') as mock_current_user:
+            with patch.object(utils, 'redirect') as mock_redirect:
+                @utils.login_required(route, request_args=True, **additional_arguments)
+                def fake_view():
+                    return fake_view_called
+
+                mock_current_user.is_authenticated = True
+                ret = fake_view()
+                assert ret == fake_view_called
+                mock_redirect.assert_not_called()
+
+    #
+    with patch.object(utils.current_app.config, 'get', return_value=False):
+        with patch.object(utils, 'current_user') as mock_current_user:
+            with patch.object(utils, 'request') as mock_request:
+                with patch.object(utils, 'redirect') as mock_redirect:
+                    with patch.object(utils, 'url_for') as mock_url_for:
+                        @utils.login_required(route, request_args=True, **additional_arguments)
+                        def fake_view():
+                            return fake_view_called
+
+                        mock_request.args = {'arg1': 'value1', 'arg2': 'value2'}
+                        mock_current_user.is_authenticated = False
+                        # (3)
+                        ret = fake_view()
+                        assert isinstance(ret, MagicMock)  # Now a new instance of mock_redirect is returned
+                        query_strings = mock_request.args
+                        query_strings.update(additional_arguments)
+                        mock_redirect.assert_called_once()
+                        mock_url_for.assert_called_once_with(route, **query_strings)
+
+                        # (4)
+                        mock_redirect.reset_mock()
+                        mock_url_for.reset_mock()
+
+                        @utils.login_required(route, request_args=False, **additional_arguments)
+                        def fake_view_2():
+                            return fake_view_called
+
+                        ret = fake_view_2()
+                        assert isinstance(ret, MagicMock)  # Now a new instance of mock_redirect is returned
+                        query_strings = additional_arguments
+                        mock_redirect.assert_called_once()
+                        mock_url_for.assert_called_once_with(route, **query_strings)
