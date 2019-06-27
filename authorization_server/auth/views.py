2884118ed455236f131883cb82849dc486c242b8
d2gex <daniel.garcia@d2garcia.com>
2019-06-27 14:56:49 +0100

    Add Authorisation code request's view to auth blueprint

--- authorization_server/auth/views.py
+++ authorization_server/auth/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect
 from authorization_server import utils, oauth_grand_type as oauth_gt
 from authorization_server.auth.forms import AuthorisationForm
 
@@ -8,9 +8,26 @@ auth = Blueprint('auth', __name__, static_folder='../static/auth')
 @auth.route('/code',  methods=['GET', 'POST'])
 @utils.login_required('frontend.login', request_args=True, grand_type='code')
 def code():
+    '''Show the user the authorisation form with the permissions required by the client or either shows an error to
+    the user on the same page or redirect the user back to the client with some error code.
+    '''
+    auth_code = oauth_gt.AuthorisationCode(request.args)
+    valid_request = auth_code.validate_request()
 
-    # RE-DO: Code just to test the login-required aspect of this view
-    auth_code = oauth_gt.AuthorisationCode(**request.args)
-    auth_code.validate_request()
+    # Is the request valid both in format and semantics => show authorisation form
+    if valid_request:
+        form = AuthorisationForm()
+        return render_template('auth/code.html', client_app=auth_code, form=form, errors=False)
+
+    # ... Or perhaps there is an error that the user should know as specified by oAuth 2.0? => show error on page
     if auth_code.errors['code'] == 400:
         return render_template('auth/code.html', errors=auth_code.errors), 400
+
+    # ...Otherwise there is an error that only concerns the client only => redirect the error
+    if auth_code.errors['code'] == 200:
+        errors = auth_code.errors
+        url = auth_code.redirect_uri
+        url += f"?error={errors['error']}&error_description={errors['error_description']}"
+        if 'state' in errors:
+            url += f"&state={errors['state']}"
+        return redirect(url)
--- tests/test_auth_blueprint.py
+++ tests/test_auth_blueprint.py
@@ -1,5 +1,7 @@
+import base64
+
 from sqlalchemy.orm import exc
-from authorization_server import models
+from authorization_server import models, oauth_grand_type as oauth_gt
 from authorization_server.app import db, bcrypt
 from tests import utils as test_utils
 
@@ -27,7 +29,21 @@ def add_user_client_context_to_db():
     db.session.add(user)
     db.session.commit()
 
+    client_data[0]['id'] = client.id
+    user_data['id'] = user.id
     return client_data, user_data
+
+
+def perform_logged_in(app_instance, user_data):
+    data = {
+        'email': user_data['email'],
+        'password': user_data['password']
+    }
+    response = app_instance.post('/login', data=data, follow_redirects=True)
+    assert response.status_code == 200
+    assert 'Account Details' in response.get_data(as_text=True)
+    with app_instance.session_transaction() as session:
+        assert 'user_id' in session
 
 
 @test_utils.reset_database()
@@ -50,6 +66,7 @@ def test_code_login_required(frontend_app):
     '''Ensure that views in auth are login-required
 
     1) When not logged in => User should be redirected to GrandType Login page
+    2) When user is logged in => User will have access to the actual rendered view
     '''
 
     client_data, user_data = add_user_client_context_to_db()
@@ -61,16 +78,102 @@ def test_code_login_required(frontend_app):
                for keyword in ['Forgot Password?', 'This application would like:'])
 
     # (2)
-    data = {
-        'email': user_data['email'],
-        'password': user_data['password']
-    }
-    response = frontend_app.post('/login', data=data, follow_redirects=True)
-    assert response.status_code == 200
-    assert 'Account Details' in response.get_data(as_text=True)
-    with frontend_app.session_transaction() as session:
-        assert 'user_id' in session
+    perform_logged_in(frontend_app, user_data)
 
     response = frontend_app.get('/auth/code')
     assert response.status_code == 400
     assert 'Bad Request' in response.get_data(as_text=True)
+
+
+@test_utils.reset_database()
+def test_code_view_400_error(frontend_app):
+    ''' Test that the authorisation request url:
+
+    1) If not provided a client_id argument => 400 error
+    2) Provide a client_id that does not exists in the local db => 400 error
+    3) If not provided a Base64_url encoded redirect_uri => 400 error
+    4) Provide a Base64_url encoded redirect_uri that does not exist in the database => 400 error
+    5) Otherwise it may be a 302 redirection
+    '''
+
+    client_data, user_data = add_user_client_context_to_db()
+
+    # log in the user
+    perform_logged_in(frontend_app, user_data)
+
+    # (1)
+    response = frontend_app.get('/auth/code')
+    assert response.status_code == 400
+    assert all([keyword in response.get_data(as_text=True)] for keyword in ['Bad Request', 'an invalid identifier'])
+
+    # (2)
+    response = frontend_app.get('/auth/code?client_id=doesnotexistindb')
+    assert response.status_code == 400
+    assert all([keyword in response.get_data(as_text=True)] for keyword in ['Bad Request', 'not registered with us'])
+
+    # (3)
+    client_id = client_data[0]['id']
+    response = frontend_app.get(f'/auth/code?client_id={client_id}&redirect_uri=nourlbaseencode')
+    assert response.status_code == 400
+    assert all([keyword in response.get_data(as_text=True)]
+               for keyword in ['Bad Request', "'redirect_uri' argument is invalid"])
+
+    # (4)
+    redirect_uri = base64.urlsafe_b64encode('https://www.idonotexist.com'.encode()).decode()
+    response = frontend_app.get(f'/auth/code?client_id={client_id}&redirect_uri={redirect_uri}')
+    assert response.status_code == 400
+    assert all([keyword in response.get_data(as_text=True)]
+               for keyword in ['Bad Request', "'redirect_uri' is not registered with us"])
+
+    # (5)
+    redirect_uri = base64.urlsafe_b64encode(client_data[0]['redirect_uri'].encode()).decode()
+    response = frontend_app.get(f'/auth/code?client_id={client_id}&redirect_uri={redirect_uri}')
+    assert response.status_code == 302
+
+
+@test_utils.reset_database()
+def test_code_view_302_error(frontend_app):
+    '''Test that the authorisation request url:
+
+    1) if response_type is not provided or unsupported => 302 redirection with errors occur
+    2) if state is not provided -> 302 redirection with errors occur
+    '''
+    client_data, user_data = add_user_client_context_to_db()
+
+    # log in the user
+    perform_logged_in(frontend_app, user_data)
+
+    # (1)
+    client_id = client_data[0]['id']
+    redirect_uri = base64.urlsafe_b64encode(client_data[0]['redirect_uri'].encode()).decode()
+    response = frontend_app.get(f'/auth/code?client_id={client_id}&redirect_uri={redirect_uri}')
+    assert response.status_code == 302
+    assert 'unsupported_response_type' in response.headers.get('Location')
+
+    # (2)
+    response_type = oauth_gt.AuthorisationCode.grand_type
+    response = frontend_app.get(f'/auth/code?client_id={client_id}&'
+                                f'redirect_uri={redirect_uri}&'
+                                f'response_type={response_type}')
+    assert response.status_code == 302
+    assert all([keyword in response.headers.get('Location')] for keyword in ['state', 'unsupported_response_type'])
+
+
+@test_utils.reset_database()
+def test_code_view_200_successfully(frontend_app):
+
+    client_data, user_data = add_user_client_context_to_db()
+
+    # log in the user
+    perform_logged_in(frontend_app, user_data)
+    client_id = client_data[0]['id']
+    redirect_uri = base64.urlsafe_b64encode(client_data[0]['redirect_uri'].encode()).decode()
+    response_type = oauth_gt.AuthorisationCode.grand_type
+    state = 'Something the client sent in first instance'
+
+    response = frontend_app.get(f'/auth/code?client_id={client_id}&'
+                                f'redirect_uri={redirect_uri}&'
+                                f'response_type={response_type}&'
+                                f'state={state}')
+    assert response.status_code == 200
+    assert all([keyword in response.get_data(as_text=True)] for keyword in ['This application would like:', 'Allow', 'Cancel'])
